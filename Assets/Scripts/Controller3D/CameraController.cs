e96a4a328c7c6488491478877ebf979115d3ffd7
LucasBoe <lucas@lumotex.de>
2020-07-11 09:12:05 +0200

    added camera and input controller

---
+++ Assets/Scripts/Controller3D/CameraController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField]
+    float rotationSpeed;
+    [SerializeField]
+    float moveSpeed;
+    [SerializeField]
+    float zoomSpeed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //rotation
+        if (Input.GetKey(KeyCode.Q))
+        {
+            transform.Rotate(Vector3.up * rotationSpeed);
+        }
+
+        if (Input.GetKey(KeyCode.E))
+        {
+            transform.Rotate(Vector3.down * rotationSpeed);
+        }
+
+        //movement
+        transform.Translate(Vector3.right * Input.GetAxis("Horizontal") * moveSpeed);
+        transform.Translate(Vector3.forward * Input.GetAxis("Vertical") * moveSpeed);
+
+        //zoom
+        transform.Translate((Vector3.forward + transform.up * -1) * Input.mouseScrollDelta.y * zoomSpeed);
+    }
+}
